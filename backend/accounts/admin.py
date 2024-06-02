e863737125531fa924320d0f88847d7752911b83
Bahman Pournazari <bahmanpn@gmail.com>
2024-06-02 14:04:33 +0330

    5-user admin completed.now user creation and user change form are in admin panel and have effects on user model

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,1 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserChangeForm,UserCreationForm
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    form=UserChangeForm
+    add_form=UserCreationForm #for adding extra form we can use adding_form field
+
+    list_display=('email','phone_number','is_admin')
+    list_filter=('is_admin',)
+
+    # this is for form field
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email','phone_number','fullname','password'
+            ),
+        }),
+        ('Permissions',{
+            "fields":(
+                'is_active','is_admin','last_login'
+            )
+        })
+    )
+    
+    # this is for add_form field to show fields of UserCreation Form in admin panel
+    add_fieldsets=(
+        (None,{
+            "fields":(
+                'phone_number','email','fullname','password1','password2'
+            )
+            }),
+    )
+
+    search_fields=('email','fullname','phone_number')
+    ordering=('phone_number','fullname')
+
+    # this is for permissions that set in User model but we did not set any permission yet,
+    # so its empty but it is required to write
+    filter_horizontal=()
+
+admin.site.unregister(Group) 
+admin.site.register(User,UserAdmin)
