18b65a0dbed66e1171ba049b956644e0e9fbb6a9
Bahman Pournazari <bahmanpn@gmail.com>
2024-06-26 19:05:48 +0330

    29-add and save method of basket class added. action url of product detail form and basket add view added to project

--- backend/orders_module/basket.py
+++ backend/orders_module/basket.py
@@ -1,12 +1,34 @@
 BASKET_SESSION_ID='basket'
 
 class Basket:
+    '''
+        constructor method creates a session ,basket(if does not exists created empty dic) varibale
+
+        add method get product and quantity of that product from form and request with BasketAddView 
+
+        save method modified session because session does not save some actions auto and need modify 
+    '''
+
     def __init__(self,request):
         self.session=request.session
         basket=self.session.get(BASKET_SESSION_ID)
         
-        # if basket is None:
-        if not basket:
+        if not basket: # if basket is None:
             basket=self.session[BASKET_SESSION_ID]={}
         
         self.basket=basket
+
+        def add(self,product,quantity):
+            '''
+                its clear that product and quantity uses for adding a new dic in basket dictionary!! 
+                after all we need to call session to save them,for that i created new method that its name is save!!
+            '''
+            
+            product_id=str(product.id)
+            if product_id not in self.basket:
+                self.basket[product_id]={'quantity':0,'price':str(product.price)}
+            self.basket[product_id]['quantity']+=quantity
+            self.save()
+
+        def save(self):
+            self.session.modified=True
--- backend/orders_module/views.py
+++ backend/orders_module/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views import View
+from product_module.models import Product
+from .forms import AddBasketForm
+from .basket import Basket
+from django.contrib import messages
 
 class BasketView(View):
     template_name="orders_module/basket.html"
@@ -8,5 +12,19 @@ class BasketView(View):
         return render(request,self.template_name)
     
 class BasketAddView(View):
+    '''
+        post method comes from product detail view form that has a integer field,
+        with basket.py and sessions we can add a product to basket and
+        show them in table of basket.html and it uses in BasketView. 
+    '''
     def post(self,request,product_id):
-        pass
+        basket=Basket(request)
+        product=get_object_or_404(Product,id=product_id)
+        form=AddBasketForm(request.POST)
+
+        if form.is_valid():
+            basket.add(product,form.cleaned_data['quantity'])
+
+        messages.success(request,'product added to basket successfully!!',extra_tags='success')
+        return redirect('product_module:products')
+        # return redirect('product_module:product-detail',product.slug)
