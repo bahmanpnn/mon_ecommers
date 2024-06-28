2d11a1be16070d309bd0bad5f51ad4ed34aba40a
Bahman Pournazari <bahmanpn@gmail.com>
2024-06-28 16:52:06 +0330

    33-Order and OrderItem models with admin models added.there are OrderItem items in Order page like table

--- backend/orders_module/admin.py
+++ backend/orders_module/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order,OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model=OrderItem
+    raw_id_fields=('product',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    '''
+        this is main admin page that use OrderModel and 
+        after data of Order model we can see Order Item model like table and
+        default has 3 item in table that every item is one order item
+    '''
+    list_display=('id','user','updated_date','is_paid')
+    list_filter=('is_paid',)
+    inlines=(OrderItemInline,) 
+
+
+# admin.site.register(Order,OrderAdmin)
--- backend/orders_module/models.py
+++ backend/orders_module/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+'''
+    this models using when user wants to pay and models get their datas from session that
+    filled before with basket session.
 
-# Create your models here.
+    we can get user with 3 ways==>
+    1- from django.contrib.auth import User -->if changed user model --> from accounts.models import User
+    2- from django.conf import settings --> settings.AUTH_USER_MODEL
+    3- from django.contrib.auth import get_user_model
+
+'''
+
+from django.db import models
+from django.contrib.auth import get_user_model
+from product_module.models import Product
+
+
+User=get_user_model()
+
+class Order(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='orders')
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    is_paid=models.BooleanField(default=False)
+
+    class Meta:
+        ordering=('is_paid','-created_date','-updated_date')
+
+    def __str__(self):
+        return f'{self.user} - {self.is_paid}'
+    
+    def get_total_price(self):
+        return sum(item.get_cost() for item in self.order_items.all())
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order,on_delete=models.CASCADE,related_name='order_items')
+    product=models.ForeignKey(Product,on_delete=models.PROTECT)
+    price=models.PositiveIntegerField()
+    quantity=models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.id
+    
+    def get_cost(self):
+        return self.price * self.quantity
